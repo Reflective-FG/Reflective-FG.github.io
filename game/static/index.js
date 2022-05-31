7fd76eb5b23cc67cd648973ca566f1a762f05b1e
Reflective-FG <1298795058@qq.com>
2022-05-31 22:17:20 +0800

    Site updated: 2022-05-31 22:17:19

--- game/static/index.js
+++
@@ -1,6 +0,0 @@
-let isDesktop = navigator['userAgent'].match(/(ipad|iphone|ipod|android|windows phone)/i) ? false : true;
-let fontunit = isDesktop ? 20 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
-document.write('<style type="text/css">' +
-    'html,body {font-size:' + (fontunit < 30 ? fontunit : '30') + 'px;}' +
-    (isDesktop ? '#welcome,#GameTimeLayer,#GameLayerBG,#GameScoreLayer.SHADE{position: absolute;}' :
-        '#welcome,#GameTimeLayer,#GameLayerBG,#GameScoreLayer.SHADE{position:fixed;}@media screen and (orientation:landscape) 
